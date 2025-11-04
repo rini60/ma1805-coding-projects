59f43cf81c27725afe4da62b57d9423cfc2debda
rini60 <harini0607@outlook.com>
2025-11-04 12:01:56 +0000

    sketch.js

--- 03multiples/sketch.js
+++ 03multiples/sketch.js
@@ -1,5 +1,5 @@
 let rgb = [ 255,0, 0,] ;
-let select = 0
+let select = 0;
 
 function setup() {
   createCanvas(400, 400);
@@ -11,21 +11,24 @@ function draw() {
   fill (rgb);
   if (select == 1) {
     rgb = [0, 255, 0]
-  }else{
+  } else {
     rgb = [255, random(255), 0]
   }
 
-  for(let i=0; i<=100; i=i+3) {
-    r = random(10)
-    circle(120+r+mouseX,30+i,i) ;
-
+  for (let i = 0; i<=100; i += 3) {
+    let r = random(10);
+    circle(120 + r + mouseX, 30+i, i) ;
   }
-  let words = {"a", "list", "of", "words"}
-  textSize (30)
-  text(words[3],10,50)
+
+  let words = ["a", "list", "of", "words"]
+  textSize (30);
+  text(words[3],10,50);
   }
 
   function mouseClicked () {
-    if (select == 1)
-  }
-
+    if (select == 1) {
+      select = 0;
+     } else {
+        select = 1;
+      }
+    }
