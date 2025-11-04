ad241a42f33c85225670b5be358dc769455da58c
rini60 <harini0607@outlook.com>
2025-11-04 11:21:57 +0000

    sketch.js

--- 01Drawing/sketch.js
+++ 01Drawing/sketch.js
@@ -1,5 +1,3 @@
-
-
 function setup() {
   createCanvas(850, 850, WEBGL); // Using WEBGL for 3D rendering
   colorMode(HSB, 360, 100, 100); 
@@ -17,20 +15,18 @@ rotateX(70) ;
 
   // creating points on surface of a circle using the polar coordinates
    
-  beginShape(POINTS)
-  for(theta = 0; theta; theta < 60; theta += 1){ 
-    for (let  phi = 0; phi < 360 phi += 2) {  
-    let r = (70 * pow(abs(sin(phi * *8/2)), 1) + 250) * theta/60; //multiply phi with arbitrary value, move sin wave up to create my prefered shape, added powers to edit the sharpness of the petals, makes shape more dense and flat when multipying the radius by theta/60
+  beginShape(POINTS);
+  for(theta = 0; theta < 60; theta += 1){ 
+    for (let  phi = 0; phi < 360; phi += 2) {  
+    let r = (70 * pow(abs(sin(phi * 5/2)), 1) + 250) * theta/60; //multiply phi with arbitrary value, move sin wave up to create my prefered shape, added powers to edit the sharpness of the petals, makes shape more dense and flat when multipying the radius by theta/60
     let x = r * cos (phi); 
     let y = r * sin(phi); 
     let bumpiness = 2 * pow(r/100, 2) * sin(phi * 12); //added bumpiness to make flower more natual looking
     let z = 300 * pow(Math.E, -0.15 * pow(abs(r/100), 1.5)) * pow(abs(r/100), 0.8)-200 + bumpiness; //added exponential decay and power function to create a more interesting z axis
-    
-    let pos = createVector(x, y, z);
-    v[theta].push(pos); 
-
+    vertex(x, y, z) 
     }
-  } 
-
-    }
+  }
   endShape();
+
+    }
+
